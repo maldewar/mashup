90d4778b5913a00855787b0b48ff61e3a62e97df
maldewar <valdemar@gmail.com>
2014-02-24 16:50:24 -0700

    Wrap macros for logging functions.

--- src/util/Log.cc
+++ src/util/Log.cc
@@ -6,26 +6,38 @@
 #include <log4cxx/consoleappender.h>
 #include <log4cxx/patternlayout.h>
 
-log4cxx::LoggerPtr Log::_logger = log4cxx::Logger::getLogger("Mashup");
+bool Log::_is_init = false;
 
-void Log::Trace(const std::string msg) {
+void Log::Trace(const std::string msg, const std::string logger_name) {
+  LOG4CXX_TRACE(GetLogger(logger_name), msg);
 };
 
-void Log::Debug(const std::string msg) {
+void Log::Debug(const std::string msg, const std::string logger_name) {
+  LOG4CXX_DEBUG(GetLogger(logger_name), msg);
 };
 
-void Log::Info(const std::string msg) {
+void Log::Info(const std::string msg, const std::string logger_name) {
+  LOG4CXX_INFO(GetLogger(logger_name), msg);
 };
 
-void Log::Warn(const std::string msg) {
+void Log::Warn(const std::string msg, const std::string logger_name) {
+  LOG4CXX_WARN(GetLogger(logger_name), msg);
 };
 
-void Log::Error(const std::string msg) {
+void Log::Error(const std::string msg, const std::string logger_name) {
+  LOG4CXX_ERROR(GetLogger(logger_name), msg);
 };
 
-void Log::Fatal(const std::string msg) {
+void Log::Fatal(const std::string msg, const std::string logger_name) {
+  LOG4CXX_FATAL(GetLogger(logger_name), msg);
 };
 
-void Log::Message(const std::string msg, int error_level) {
-  std::cout << msg;
+log4cxx::LoggerPtr Log::GetLogger(const std::string logger_name) {
+  if (! _is_init) {
+    log4cxx::LoggerPtr rootlogger = log4cxx::Logger::getRootLogger();
+    rootlogger->addAppender(new log4cxx::ConsoleAppender(new log4cxx::PatternLayout("[%-5p] %m%n")));
+    _is_init = true;
+  }
+  log4cxx::LoggerPtr logger = log4cxx::Logger::getLogger(logger_name);
+  return logger;
 };
--- src/util/Log.h
+++ src/util/Log.h
@@ -2,28 +2,115 @@
 #define MASHUP_UTIL_LOG_
 
 #include <string>
+#include <sstream>
 
 #include <log4cxx/logger.h>
 
+const std::string LOGGER_PARSER = "common.parser";
+
+#define LOG_TRACE(msg, logger_name) { \
+  std::ostringstream oss; \
+  oss << msg; \
+  Log::Trace(oss.str(), logger_name); }
+
+#define LOG_DEBUG(msg, logger_name) { \
+  std::ostringstream oss; \
+  oss << msg; \
+  Log::Debug(oss.str(), logger_name); }
+
+#define LOG_INFO(msg, logger_name) { \
+  std::ostringstream oss; \
+  oss << msg; \
+  Log::Info(oss.str(), logger_name); }
+
+#define LOG_WARN(msg, logger_name) { \
+  std::ostringstream oss; \
+  oss << msg; \
+  Log::Warn(oss.str(), logger_name); }
+
+#define LOG_ERROR(msg, logger_name) { \
+  std::ostringstream oss; \
+  oss << msg; \
+  Log::Error(oss.str(), logger_name); }
+
+#define LOG_FATAL(msg, logger_name) { \
+  std::ostringstream oss; \
+  oss << msg; \
+  Log::Fatal(oss.str(), logger_name); }
+
+/**
+ * @file
+ * @brief Logging class.
+ *
+ * This class is intended to work as a wrapper class for
+ * Log4Cxx. http://logging.apache.org/log4cxx/
+ * @author Waldemar Sánchez
+ * @date February, 2014
+ */
 class Log {
   public:
-    static void Trace(const std::string msg);
-    static void Debug(const std::string msg);
-    static void Info(const std::string msg);
-    static void Warn(const std::string msg);
-    static void Error(const std::string msg);
-    static void Fatal(const std::string msg);
+    /**
+     * Log function for trace messages. Lowest level for logging.
+     * Used mainly on development environment to inspect program
+     * flow.
+     * @param msg         Message to log.
+     * @param logger_name Name of the logger.
+     */
+    static void Trace(const std::string msg, const std::string logger_name);
+    /**
+     * Log function for debug messages. Useful for debuging and testing.
+     * @param msg         Message to log.
+     * @param logger_name Name of the logger.
+     */
+    static void Debug(const std::string msg, const std::string logger_name);
+    /**
+     * Log function for information messages. Used for logging useful
+     * information about the normal flow of the application.
+     * @param msg         Message to log.
+     * @param logger_name Name of the logger.
+     */
+    static void Info(const std::string msg, const std::string logger_name);
+    /**
+     * Log function for warning messages. Used for informing about abnormal
+     * states the application itself try to solve so it does not generate
+     * an error later.
+     * @param msg         Message to log.
+     * @param logger_name Name of the logger.
+     */
+    static void Warn(const std::string msg, const std::string logger_name);
+    /**
+     * Log function for error messages. Error messages compromise processes
+     * but the application is still able to run.
+     * @param msg         Message to log.
+     * @param logger_name Name of the logger.
+     */
+    static void Error(const std::string msg, const std::string logger_name);
+    /**
+     * Log function for fatal messages. Fatal messages are total blockers for
+     * for the application, not able to run on these situations.
+     * @param msg         Message to log.
+     * @param logger_name Name of the logger.
+     */
+    static void Fatal(const std::string msg, const std::string logger_name);
 
   private:
     /**
-     * Class constructor.
+     * Private constructor for static class.
      */
     Log();
-    static void Message(const std::string msg, int error_level);
+    /**
+     * Get a LoggerPtr given the name of a logger.
+     * @param logger_name Logger name.
+     * @return LoggerPtr to be used with Log4Cxx macros.
+     */
+    static log4cxx::LoggerPtr GetLogger(const std::string logger_name);
 
   private:
-    static log4cxx::LoggerPtr _logger;
-    
+    /**
+     * Constrols static initialization for Log4Cxx.
+     */
+    static bool _is_init;
+
 };
 
 #endif // MASHUP_UTIL_LOG_
